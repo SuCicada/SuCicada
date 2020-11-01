97301fdb25258d9c76f0bfa440048ca7fd611b22
SuCicada <pengyifu@gmail.com>
2020-11-02 00:32:46 +0800

    2020/11/01

--- web/sucicada.js
+++ web/sucicada.js
@@ -1,36 +1,51 @@
-script = document.createElement('script')
-script.src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js"
-document.getElementsByTagName('head')[0].appendChild(script);
+IMPORT_JS = [""]
 
-
-// Sucicada = {
-// }
-
-
-
-function loadMD(mdList) {
-    mdList
-        .forEach(function (md) {
-            addMD(md)
-        })
-
-    MDBuffer = []
+function loadJS(resolve) {
+    script = document.createElement('script')
+    script.src = "https://cdn.jsdelivr.net/npm/marked/marked.min.js"
+    document.getElementsByTagName('head')[0].appendChild(script);
     script.addEventListener('load', function () {
-        MDBuffer.forEach(function (md) {
-            document.getElementById(md[1]).innerHTML = marked(md[0])
-        })
+        resolve()
     })
-
-    function addMD(md) {
-        let xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                let src = xmlhttp.responseText
-                MDBuffer.push([src, md[1]])
-            }
-        }
-        xmlhttp.open('GET', md[0]);
-        xmlhttp.send();
-        return this
-    }
 }
+
+function loadMD(resolve, md) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            let src = xmlHttp.responseText
+            console.log(md[1], src)
+            // console.log(resolve.toString())
+            resolve([md[1], src])
+            // resolve(src, md[1])
+        }
+    }
+    xmlHttp.open('GET', md[0]);
+    xmlHttp.send();
+    return this
+}
+
+
+function getPromise(func, ...args) {
+    return new Promise(function (resolve) {
+        func(resolve, ...args)
+    })
+}
+
+var loadJSPromise = getPromise(loadJS)
+
+function addMD(mdList) {
+    mdList
+        .forEach((md) =>
+            loadJSPromise
+                .then(() => getPromise(loadMD, md))
+                .then((md) => {
+                    document.getElementById(md[0]).innerHTML =
+                        marked(md[1], {
+                            breaks: true
+                            , gfm: true
+                        })
+                })
+        )
+}
+
---
+++ web/task/summary.md
@@ -0,0 +1,23 @@
+# short term
+## todo 
+- [ ] telegram bot api 
+- [ ] aliyun v2ray client
+- [ ] this blog
+- [ ] Promise dag module
+
+## todo [ log ]
+
+# long term
+## todo [ habit ]
+- [ ] write anything in this website
+- [ ] get rid of the comfort zone that is chinese
+
+## todo [ knowledge ]
+### computer
+- [ ] operation system 
+- [ ] c and c++
+- [ ] other language
+
+### psychology
+- [ ] **_Foundations of Social Psychology_**. then:
+- [ ] determine the next specific branch
